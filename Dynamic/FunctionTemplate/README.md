52c24cb160866812cbbd2092f475423a2646de9e
farshadsafavi <farshadtom2003@gmail.com>
2019-01-14 16:10:47 -0500

    Add example of function templates

---
+++ Dynamic/FunctionTemplate/README.md
@@ -0,0 +1,8 @@
+# Funtion Template
+- Funtion templates are used for defining generic functions.
+- they work for multiple data types.
+- Datatype is decided based on the type of value passed.
+- Datatype is a template variable
+- Function cam have multiple template variables
+
+
---
+++ Dynamic/FunctionTemplate/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+template<class T>
+
+T maxim(T a, T b){
+    return a>b ? a : b;
+}
+
+int maxim(int a, int b){
+    return a>b ? a : b;
+}
+
+float maxim(float a, float b){
+    return a>b ? a : b;
+}
+
+int main()
+{
+    cout << "Template Function" << endl;
+    cout << maxim(12, 14) << endl;
+    cout << maxim(2.3, 1.4) << endl;
+    cout << maxim(2.3f, 5.6f) << endl;
+    return 0;
+}
